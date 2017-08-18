9dd024af3c5654202e49b481eb922a3787055428
Nathaniel Schweinberg <natanielks@gmail.com>
2017-08-18 13:07:48 -0700

    fix incorrect filenames and namespaces in filters.php

--- filters.php
+++ filters.php
@@ -1,9 +1,9 @@
 <?php
 
 require_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
-require_once( dirname( __FILE__ ) . '/class-wp-predis-client.php' );
+require_once( dirname( __FILE__ ) . '/class-wp-predis-decorator.php' );
 require_once( dirname( __FILE__ ) . '/functions.php' );
 
 add_filter( 'wp_redis_check_client_dependencies_callback', 'WP_Predis\check_client_dependencies_callback' );
-add_filter( 'wp_redis_client_connection_callback', 'WP_Predis\WP_Predis\client_connection_callback' );
+add_filter( 'wp_redis_client_connection_callback', 'WP_Predis\client_connection_callback' );
 add_filter( 'wp_redis_setup_client_connection_callback', 'WP_Predis\setup_client_connection_callback' );
