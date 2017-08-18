97ed0901446e3717bc2340a52b7eae9be7218087
Nathaniel Schweinberg <natanielks@gmail.com>
2017-08-18 13:14:14 -0700

    enable configuration of wordpress tests database connection

--- bin/setup-wp-redis.sh
+++ bin/setup-wp-redis.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 WP_REDIS=/tmp/nathanielks-wp-redis
+MYSQL_DB="${MYSQL_DB:-wordpress_test}"
+MYSQL_USER="${MYSQL_USER:-root}"
+MYSQL_PASS="${MYSQL_PASS:-}"
+MYSQL_HOST="${MYSQL_HOST:-localhost}"
 
 git clone -b make-redis-pluggable --single-branch --depth 1 https://github.com/nathanielks/wp-redis.git ${WP_REDIS}
 
 cd "${WP_REDIS}" || exit
 composer install
-bash bin/install-wp-tests.sh wordpress_test root root localhost latest
+bash bin/install-wp-tests.sh "${MYSQL_DB}" "${MYSQL_USER}" "${MYSQL_PASS}" "${MYSQL_HOST}" latest
